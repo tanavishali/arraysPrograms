87d82438c4eed53b5a634e500714097da48a0adc
tanavishali <tanawishalrai5271@gmail.com>
2024-03-24 15:05:04 +0500

    first commit

---
+++ Initialize and declar of array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	we use arrays to store the data of same data typs 
+	int arr[3]; //Initialize 
+	arr[0]={1}; //declar the array 
+	arr[1]={2}; 
+	arr[2]={3};
+//	index of array start from 0 to n
+	cout<<arr[2]; //we can acces the array element by using  index of array  
+}
