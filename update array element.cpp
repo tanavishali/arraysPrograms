c3b20b36d6087e5b46406f18ad483706905970a8
tanavishali <tanawishalrai5271@gmail.com>
2024-03-24 20:48:19 +0500

    new program

---
+++ update array element.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// update the array element 
+	int update[4]={1,2,0,4};
+	cout<<"the value of index 2 is :"<<update[2]<<endl;
+	cout<<" update the index 2 then the elemnt value  is :";
+	update[2]={3};
+	cout<<update[2];
+}
